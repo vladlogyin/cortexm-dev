83537b35f4ef57654e2625a5a32dece2bd820842
Vlad Logyin <vladlogyin@gmail.com>
2020-09-24 19:40:36 +0100

    remove unnecesary include

--- ili9341/ili9341.h
+++ ili9341/ili9341.h
@@ -3,7 +3,6 @@
 #include <systems.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/dma.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/spi.h>
